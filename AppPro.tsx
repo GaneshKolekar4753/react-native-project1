77d380a32ed25fe6200b0c767bca49b0f46b52e3
Ganesh Kolekar <kolekarganesh5151@gmail.com>
2024-02-29 22:25:49 +0530

    hellow world

---
+++ AppPro.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {View,SafeAreaView,Text} from 'react-native'
+
+function AppPro(){
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default AppPro;
