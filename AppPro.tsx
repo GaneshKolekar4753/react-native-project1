45c721fb90ac0de827d707a298305a51588ef347
Ganesh Kolekar <kolekarganesh5151@gmail.com>
2024-02-29 23:06:24 +0530

    added styles and usecolorscheme hook

--- AppPro.tsx
+++ AppPro.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 
-import {View,SafeAreaView,Text} from 'react-native'
+import {View, SafeAreaView, Text, StyleSheet,useColorScheme} from 'react-native';
 
-function AppPro(){
-    return(
-        <SafeAreaView>
-            <View>
-                <Text>Hello World</Text>
-            </View>
-        </SafeAreaView>
-    )
+function AppPro():JSX.Element {
+    const isDarkmode=useColorScheme()==='dark';
+  return (
+    <SafeAreaView  style={styles.container}>
+      <View>
+        <Text style={isDarkmode?styles.whitetext:styles.darkText}>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center'
+    },
+    whitetext:{
+        color:'#FFFFFF'
+    },
+    darkText:{
+        color:'#000000'
+    }
+})
 
 export default AppPro;
